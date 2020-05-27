47df1c33e85258fc5af2f47c78bf050279029307
charliechauri <me@charliechauri.com>
2020-05-27 02:20:46 -0500

    feat: create website v1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+/** @todo Add coverage */
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByAltText } = render(<App />);
+  const logo = getByAltText(/charliechauri's logo/i);
+  expect(logo).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from 'img/logo.png';
 import './App.scss';
+
+/** @todo move to its own file */
+const socialNetworks = [
+  {
+    name: 'github',
+    href: ' https://github.com/charliechauri/',
+  },
+  {
+    name: 'linkedin',
+    href: 'https://www.linkedin.com/in/carlosechauri/',
+  },
+  {
+    name: 'twitter',
+    href: 'https://twitter.com/charliechauri/',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className="app">
+      <header className="header">
+        <h1 className="screen-reader-only">charliechauri software engineer</h1>
+
+        {/* @todo create logo component */}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/carlosechauri/"
+          className="app__logo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img
+            src={logo}
+            className="app__logo-img"
+            alt="charliechauri's logo"
+          />
         </a>
       </header>
+
+      {/* @todo create description component */}
+      <section className="app__description">
+        <p>
+          I'm a <mark>Software Engineer</mark> with experience in different
+          industries (travel technology, human resources, legal services and
+          energy) with global projects and remote teams.
+        </p>
+      </section>
+
+      {/* @todo create social networks component and children */}
+      <section className="app__social-networks">
+        <h2 className="app__social-networks-heading">Follow me</h2>
+        <ul className="app__social-networks-list">
+          {socialNetworks.map(({ href, name }) => (
+            <li key={name} className="app__social-networks-list-item">
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                {name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
