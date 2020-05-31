cb19969d20625e5615d96bbdcf1450937918d58e
charliechauri <me@charliechauri.com>
2020-05-31 06:17:30 -0500

    style: remove unneeded variable

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 /** @todo Add coverage */
 test('renders learn react link', () => {
-  const { getByAltText, getByText, container } = render(<App />);
+  const { getByAltText, getByText } = render(<App />);
   const logo = getByAltText(/logo_alt_text/i);
   const languageSwitcher = getByText('English');
 
