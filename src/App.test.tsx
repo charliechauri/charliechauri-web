642476e6644826af8be80d585cfc31e2f3668d18
charliechauri <me@charliechauri.com>
2020-05-31 06:42:34 -0500

    test: update main test to have enough coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18n';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 /** @todo Add coverage */
 test('renders learn react link', () => {
-  const { getByAltText, getByText } = render(<App />);
-  const logo = getByAltText(/logo_alt_text/i);
-  const languageSwitcher = getByText('English');
+  const { getByAltText, getByLabelText } = render(
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  );
+  const logo = getByAltText(/charliechauri's logo/i);
+  const languageSwitcher = getByLabelText('language_switcher_indications');
 
-  fireEvent.click(languageSwitcher);
-
-  const spanishOption = getByText('Español');
-
-  fireEvent.click(spanishOption);
+  fireEvent.change(languageSwitcher, { value: 'es' });
 
   expect(logo).toBeInTheDocument();
-  expect(spanishOption).toBeInTheDocument();
 });
