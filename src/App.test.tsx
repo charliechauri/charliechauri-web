11ca150bc6e5a1618128b070644f0d06ce092e45
charliechauri <me@charliechauri.com>
2020-05-31 06:16:32 -0500

    test: increase coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,13 +4,15 @@ import App from './App';
 
 /** @todo Add coverage */
 test('renders learn react link', () => {
-  const { getByAltText, getByText } = render(<App />);
+  const { getByAltText, getByText, container } = render(<App />);
   const logo = getByAltText(/logo_alt_text/i);
   const languageSwitcher = getByText('English');
 
   fireEvent.click(languageSwitcher);
 
   const spanishOption = getByText('Español');
+
+  fireEvent.click(spanishOption);
 
   expect(logo).toBeInTheDocument();
   expect(spanishOption).toBeInTheDocument();
