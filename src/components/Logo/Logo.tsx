6244d93d09822b8c28888e6ff90bb412e91d973b
charliechauri <me@charliechauri.com>
2020-06-14 14:16:16 -0500

    feat: add support for dark theme

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import ThemeContext from 'ThemeContext';
+
 import './Logo.scss';
-import logo from 'img/logo.png';
+import lightLogo from 'img/logo.png';
+import darkLogo from 'img/logo_dark.png';
+
+const logos = {
+  light: lightLogo,
+  dark: darkLogo,
+};
 
 export interface LogoProps {
   href: string;
@@ -12,9 +20,24 @@ const Logo: FC<LogoProps> = ({ href }) => {
   const { t } = useTranslation();
 
   return (
-    <a href={href} className="logo" target="_blank" rel="noopener noreferrer">
-      <img src={logo} className="logo__img" alt={t('logo_alt_text')} />
-    </a>
+    <ThemeContext.Consumer>
+      {({ theme }) => {
+        return (
+          <a
+            href={href}
+            className="logo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={logos[theme]}
+              className="logo__img"
+              alt={t('logo_alt_text')}
+            />
+          </a>
+        );
+      }}
+    </ThemeContext.Consumer>
   );
 };
 
