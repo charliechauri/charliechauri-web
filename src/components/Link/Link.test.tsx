fb636f5a01aac79759f3c85f4ea39a5e4d2286c7
charliechauri <me@charliechauri.com>
2020-06-23 23:43:40 -0500

    refactor: add ThemeTestWrapper to test/utils
    - Update Link test to import correctly
    
    Fixes: #61

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import ThemeTestWrapper from 'test/ThemeTestWrapper';
+import { ThemeTestWrapper } from 'test/utils';
 import Link, { TargetTypes } from './Link';
 
 describe('Link', () => {
--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,3 +1,6 @@
 import I18nTestWrapper from 'test/I18nTestWrapper';
+import ThemeTestWrapper from 'test/ThemeTestWrapper';
 
 export { I18nTestWrapper };
+
+export { ThemeTestWrapper };
